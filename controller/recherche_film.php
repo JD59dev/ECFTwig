2e9ca7626cbc9a4f999ab53aec0920659db9e978
daremma <119937508+daremma@users.noreply.github.com>
2023-07-20 14:58:16 +0200

    new failure RECHERCHE FILM

--- controller/recherche_film.php
+++ controller/recherche_film.php
@@ -3,7 +3,7 @@
 $movieDao = new FilmsDAO();
 $chercheMovie = $movieDao->searchFilm();
 
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $titre = $_GET['titre'];
     $movie = new Film($id, $titre, $realisateur, $affiche, $annee);   
     $chercheMovie = $movieDao->searchFilm($titre);
