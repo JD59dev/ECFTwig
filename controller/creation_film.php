78e98cf6b8fb243b25d4a0586f92e160ddd69308
Fredbarillon <barillondev@gmail.com>
2023-07-25 13:46:12 +0200

    Revert "Merge branch 'main' of https://github.com/JD59dev/ECFTwig"
    This reverts commit c6c2680ce49d63dbc440f7652df208499cd2b552, reversing
    changes made to 71249bd9bdbf20bed8388e8a531b100dd37885dd.

--- controller/creation_film.php
+++ controller/creation_film.php
@@ -1,37 +1,27 @@
 <?php
 
+$filmDao = new FilmsDAO();
+$message = "";
+$film = null;
 
-if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
-    header("Location: login"); 
-    }else{
-        $filmDao = new FilmsDAO();
-        $message = "";
-        $film = null;
-        
-        if ($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST['titre']) && !empty($_POST['realisateur']) && !empty($_POST['affiche']) && !empty($_POST['annee']) && !empty($_POST['roles'])) {
-                if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
-                // L'utente non è autenticato, quindi reindirizzalo alla pagina di login
-                header("Location: /login"); // Sostituisci "/login" con l'URL della tua pagina di login
-                exit();
-            $titre = $_POST['titre'];
-                $realisateur = $_POST['realisateur'];
-                $affiche = $_POST['affiche'];
-                $annee = $_POST['annee'];
-                $roles = $_POST['roles'];
-        
-                $film = new Film(null, $titre, $realisateur, $affiche, $annee, $roles);
-                $ajouter = $filmDao->add($film);
-        
-                if ($ajouter) {
-                    $message =  "Ajout OK";
-                } else {
-                    $message = "Erreur Ajout";
-                }
-            } else {
-                $message = "Tous les champs du formulaire doivent être remplis.";
-            }
-        
-        echo $twig->render('creation_film.html.twig', ['erreur' => $message]);
+if ($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST['titre']) && !empty($_POST['realisateur']) && !empty($_POST['affiche']) && !empty($_POST['annee']) && !empty($_POST['roles'])) {
+        $titre = $_POST['titre'];
+        $realisateur = $_POST['realisateur'];
+        $affiche = $_POST['affiche'];
+        $annee = $_POST['annee'];
+        $roles = $_POST['roles'];
+
+        $film = new Film(null, $titre, $realisateur, $affiche, $annee, $roles);
+        $ajouter = $filmDao->add($film);
+
+        if ($ajouter) {
+            $message =  "Ajout OK";
+        } else {
+            $message = "Erreur Ajout";
         }
-        
+    } else {
+        $message = "Tous les champs du formulaire doivent être remplis.";
     }
+
+
+echo $twig->render('creation_film.html.twig', ['erreur' => $message]);
